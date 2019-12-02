640c1bf31582c651db0225842f51af597614d38a
andrew <andrew@DESKTOP-77O1GIB>
2019-12-02 13:33:52 +0000

    Add project files.

---
+++ WordWrapping/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WordWrapping;
+
+namespace WordWrapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wrapping wrap = new Wrapping();
+
+            string result = wrap.Wrap("Hello there, how are you today?", 10);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ WordWrapping/Wrapping.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordWrapping
+{
+    class Wrapping
+    {
+        public string Wrap(string input, int width) 
+        {
+            int length = input.Length;
+
+            string output = string.Empty;
+            string[] words = input.Split(' ');
+
+            if (length > width)
+            {
+                int count = 0;
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    int outputLength = words[i].Length;
+
+                    if (outputLength + count <= width) 
+                    {
+                        output += words[i] + ' ';
+                        count += outputLength + 1;
+                    }
+
+                    else
+                    {
+                        output += "\n" + words[i] + ' ';
+                        count = 0 + outputLength + 1;
+                    }
+                }
+            }
+
+            else 
+            {
+                output = input;
+            }
+
+            return output;
+        }
+    }
+}
